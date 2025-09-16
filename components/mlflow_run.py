baa659a5d75a034eb5c386252e169f6a3e8c0e0c
Prashal Ruchiranga <prashalruchiranga@gmail.com>
2025-09-16 18:05:17 +0530

    Define a component to start an MLflow run and return the run ID for downstream components

---
+++ components/mlflow_run.py
@@ -0,0 +1,21 @@
+from kfp.dsl import component
+
+@component(
+    base_image="docker.io/prashalruchiranga/news-classifier:components-v1.1",
+    packages_to_install=["mlflow==3.3.2", "google-cloud-storage==3.3.1"],
+)
+def get_run_id(
+    mlflow_tracking_uri: str,
+    experiment_name: str
+    ) -> str:
+    import os
+    import mlflow
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/etc/gcs/key.json"
+
+    mlflow.set_tracking_uri(mlflow_tracking_uri)
+    mlflow.set_experiment(experiment_name)
+    run = mlflow.start_run()
+    run_id = run.info.run_id
+    mlflow.end_run()
+    return run_id
