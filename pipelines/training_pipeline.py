cf3d3a092f62907d4fed4821ab4b1c25336d1482
Prashal Ruchiranga <prashalruchiranga@gmail.com>
2025-09-16 22:33:09 +0530

    Mount gcs-credentials as volume in every component

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,6 +1,6 @@
 from kfp import kubernetes
 from kfp.dsl import pipeline
-from components import data_loader, model_trainer, model_evaluator
+from components import mlflow_run, data_loader, model_trainer, model_evaluator
 from typing import Optional
 
 @pipeline(
@@ -9,6 +9,7 @@ from typing import Optional
 )
 def training_pipeline(
     mlflow_tracking_uri: str,
+    experiment_name: str,
     dataset_name: str,
     text_column_name: str,
     label_column_name: str,
@@ -19,20 +20,37 @@ def training_pipeline(
     learning_rate: float = 5e-5,
     epochs: int = 20,
     early_stopping_min_delta: float = 0.001,
-    max_no_improve_epochs: int = 5,
+    early_stopping_patience: int = 5,
     debug_batch_count: Optional[int] = None
     ):
+    get_run_id_op = mlflow_run.get_run_id(
+        mlflow_tracking_uri=mlflow_tracking_uri,
+        experiment_name=experiment_name
+    )
+    kubernetes.use_secret_as_volume(
+        task=get_run_id_op,
+        secret_name="gcs-credentials",
+        mount_path="/etc/gcs"
+    )
+    get_run_id_op.set_caching_options(False)
+    run_id = get_run_id_op.output
+
     load_dataset_op = data_loader.load_hf_dataset(
         mlflow_tracking_uri=mlflow_tracking_uri,
-        name=dataset_name, 
+        mlflow_run_id=run_id,
+        name=dataset_name,
         val_fraction=val_data_fraction,
         seed=dataset_split_seed
     )
-    kubernetes.use_secret_as_volume(task=load_dataset_op,
-                                    secret_name="gcs-credentials",
-                                    mount_path="/etc/gcs")
+    kubernetes.use_secret_as_volume(
+        task=load_dataset_op,
+        secret_name="gcs-credentials",
+        mount_path="/etc/gcs"
+    )
 
     finetune_bert_op = model_trainer.finetune_bert(
+        mlflow_tracking_uri=mlflow_tracking_uri,
+        mlflow_run_id=run_id,
         preset=bert_preset,
         batch_size=batch_size,
         text_col=text_column_name,
@@ -40,18 +58,31 @@ def training_pipeline(
         lr=learning_rate,
         epochs=epochs,
         min_delta=early_stopping_min_delta,
-        patience=max_no_improve_epochs,
+        patience=early_stopping_patience,
         debug_batch_count=debug_batch_count,
         train_dataset=load_dataset_op.outputs["train_dataset"],
         val_dataset=load_dataset_op.outputs["val_dataset"]
     )
+    kubernetes.use_secret_as_volume(
+        task=finetune_bert_op,
+        secret_name="gcs-credentials",
+        mount_path="/etc/gcs"
+    )
+
     evaluate_model_op = model_evaluator.evaluate_model(
+        mlflow_tracking_uri=mlflow_tracking_uri,
+        mlflow_run_id=run_id,
         batch_size=batch_size,
         text_col=text_column_name,
         label_col=label_column_name,
         debug_batch_count=debug_batch_count,
         test_dataset=load_dataset_op.outputs["test_dataset"],
         saved_model=finetune_bert_op.outputs["saved_model"]
+    )
+    kubernetes.use_secret_as_volume(
+        task=evaluate_model_op,
+        secret_name="gcs-credentials",
+        mount_path="/etc/gcs"
     )
 
 
@@ -75,6 +106,7 @@ if __name__ == "__main__":
         pipeline_func=training_pipeline,
         arguments={
             "mlflow_tracking_uri": configs.mlflow_server,
+            "experiment_name": configs.experiment,
             "dataset_name": configs.dataset,
             "text_column_name": configs.text_column,
             "label_column_name": configs.label_column,
