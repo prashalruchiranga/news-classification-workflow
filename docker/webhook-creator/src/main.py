9c8ed7f032e6d9a89c31399bcec50b5edd9537a5
Prashal Ruchiranga <prashalruchiranga@gmail.com>
2025-10-11 15:39:07 +0530

    Add functionality to update MLflow webhook

--- docker/webhook-creator/src/main.py
+++ docker/webhook-creator/src/main.py
@@ -5,15 +5,32 @@ TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
 RECEIVER_URI = os.getenv("MLFLOW_WEBHOOK_RECEIVER_URI")
 MLFLOW_WEBHOOK_SECRET = os.getenv("MLFLOW_WEBHOOK_SECRET")
 
+if not all([TRACKING_URI, RECEIVER_URI, MLFLOW_WEBHOOK_SECRET]):
+    raise ValueError("One or more required environment variables are not set")
+
 client = MlflowClient(tracking_uri=TRACKING_URI)
 
 alias_notifier = "multi-event-webhook"
 webhooks = client.list_webhooks()
-for wh in webhooks:
-    if wh.name == alias_notifier:
-        print(f"Webhook already exists: {wh.webhook_id}")
-        webhook = wh
-        break
+matching_webhooks = [wh for wh in webhooks if wh.name == alias_notifier]
+
+if len(matching_webhooks) > 1:
+    raise ValueError(f"Multiple webhooks exists with the name '{alias_notifier}'")
+elif len(matching_webhooks) == 1:
+    existing = matching_webhooks[0]
+    print(f"Webhook already exists: {existing.webhook_id}")
+    client.update_webhook(
+        webhook_id=existing.webhook_id,
+        url=RECEIVER_URI,
+        events=[
+            "model_version.created",
+            "model_version_alias.created"
+        ],
+        description="Notifies when a model version is created and assigned an alias",
+        secret=MLFLOW_WEBHOOK_SECRET
+    )
+    webhook = client.get_webhook(existing.webhook_id)
+    print(f"Updated webhook receiver: {webhook.webhook_id}")
 else:
     webhook = client.create_webhook(
         name=alias_notifier,
